f71b3d75dabdc816340e631304f82d7a2643e745
Abduramane Djaló <abduramanedjalo2006@gmail.com>
2026-09-06 21:47:43 +0000

    I'm restarting the boutique's code with my own logic.
    I deleted the old code and wrote a new code from scratch to calculate the boutique's sales.

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-# Boutique Control System by Abduramane
-# Formula: FB = SB + REV - EXP
-
-SB = input("float"()  # Starting Balance (XOF)
-REV =input("float"()  # Revenue (XOF)
-EXP = input("float"() # Expenses (XOF)
+# Boutique Control System by Abduramane Djaló 
+SB = float(input(" Starting Balance :"))
+REV = float(input(" Revenue:"))
+EXP = float(input(" Expenses:"))
 
 FB = SB + REV - EXP
-result = FB - SB
 
-print("--- BOUTIQUE CONTROL ---")
-print(f"Starting Balance: {SB} XOF")
-print(f"Revenue: {REV} XOF")
-print(f"Expenses: {EXP} XOF")
-print(f"Final Balance: {FB} XOF")
-
-if result > 0:
-    print(f"Positive! Profit: {result} XOF")
-elif result < 0:
-    print(f"Negative! Loss: {abs(result)} XOF")
+if FB > 0:
+    print("Positive XOF")
+elif FB < 0:
+    print("Negative XOF")
 else:
-    print("Neutral. No profit or loss.")
+    print("Neutral XOF")
+
