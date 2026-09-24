4f905d8956d5953a75cd1e9977194147c2c8c211
Abduramane Djaló <abduramanedjalo2006@gmail.com>
2026-09-24 22:13:18 +0000

    Creating a function and using f-strings.
    Refactor sales control logic and update input prompts.

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-# Boutique Control System by Abduramane Djaló 
-SB = float(input(" Starting Balance :"))
-REV = float(input(" Revenue:"))
-EXP = float(input(" Expenses:"))
+# Boutique_Control
 
-FB = SB + REV - EXP
+def Sales_Control(Starting_Balance, Revenue, Expenses):
 
-if FB > 0:
-    print("Positive XOF")
-elif FB < 0:
-    print("Negative XOF")
-else:
-    print("Neutral XOF")
+  Final_Balance = Starting_Balance + Revenue - Expenses
+
+  if Final_Balance > Starting_Balance:
+
+    print(f"POSITIVE! High Revenue, the value is: {Final_Balance} XOF")
+
+  elif Final_Balance < Starting_Balance:
+
+    print(f"NEGATIVE! Low Revenue, the value is: {Final_Balance} XOF")
+
+  else:
+
+    print(f"TIE! There was neither Loss nor Gain, the value is: {Final_Balance} XOF")
+
+Starting_Balance = float(input("Enter the value of Starting_Balance: "))
+Revenue = float(input("Enter the value of Revenue: "))
+Expenses = float(input("Enter the value of Expenses: ")) 
+
+Sales_Control(Starting_Balance, Revenue, Expenses)
 
